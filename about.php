d8597bf91f0d6cad48a04ee8698796a6c17cea7f
jserranos09 <64979937+jserranos09@users.noreply.github.com>
2021-03-04 13:01:50 -0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,22 @@
+<html lang="en">
+    <?php include 'inc/head.php';?>
+<body>
+    <?php include 'inc/nav.php';?>
+<!-- -fluid makes it stretch the width id the screen. container makes there always be space on both sides-->
+<header class="page-header header bod container-fluid">
+    <div class="container">
+        <div class="overlay">
+            <div class="description">
+                <h1>Learn about me</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis. Suspendisse consectetur mi id libero fringilla, in pharetra sem ullamcorper.</p>
+            </div>
+        </div>
+    </div>
+</header>
+    
+    <?php include 'inc/foot.php';?>
+    <?php include 'inc/javjq.php';?>
+</body>
+</html>
